45036d896fe1483faca9e4cd948d8cc062474ab3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-21 09:39:08 -0300

    aula python

---
+++ true ou false.py
@@ -0,0 +1,39 @@
+x = 10
+y = 5 
+# maior que 
+print (x > y) 
+
+# menor que
+print(x < y) 
+
+# maior ou igual a 
+print(x >= y) 
+
+# menor igual a 
+print(x <= y)
+
+x= 5
+y=10
+
+# and 
+print(x < 10 and y > 5)
+print(x > 10 and y < 5)
+
+# or 
+print(x < 10 or y < 5)
+print(x > 10 or y < 5)
+
+# not 
+print(not x < 10)
+print(not(x > 10))
+
+idade = 20
+
+# maior de idade 
+
+idade >= 18
+
+# menor de idade 
+idade < 18 
+not idade >= 18
+
