0157fa76c3bd5e1cbc7d978d787ed260ca594ed6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-25 09:50:32 -0300

    exercicios

--- estrutura de repetição - for/exercicio6.py
+++ estrutura de repetição - for/exercicio6.py
@@ -1,3 +1,17 @@
 '''
 Criar um algoritmo que leia dez números inteiros e informe o maior e o menor número.
 '''
+
+numero = int(input("Informe um número: "))
+menor = numero 
+maior = numero
+ 
+for i in range(9):
+    numero = int(input("Informe o número: "))
+    if numero > maior:
+        maior = numero
+    if numero < menor:
+        menor = numero2
+
+print(f'Maior: {maior}')
+print(f'Menor: {menor}')
