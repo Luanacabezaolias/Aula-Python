df5a80577cd7808cf229efaeeba9c9adb3bfbc18
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-16 09:45:38 -0300

    exercicio-lista

--- listas/exercicio1.py
+++ listas/exercicio1.py
@@ -2,11 +2,17 @@
 Solicite 10 números inteiros ao usuário e armazene os números pares em uma lista,
 e os números ímpares em outra lista.Exiba as duas listas ao usuário.
 '''
-lista = []
+pares = []
+impares = []
+
 for i in range(10):      # preenche uma lista com 10 números
     numero = int(input("Número: "))
-    lista.append(numero)
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
 
-print(lista)
+print(pares)
+print(impares)
 
 
